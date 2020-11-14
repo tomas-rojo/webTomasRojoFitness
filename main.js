7c70971fb95048cc1440bea5a9975433dcd69766
tomasrojo <tomasrojo21@hotmail.com>
2020-11-14 01:21:09 -0300

    Primer commit del Proyecto Final

---
+++ main.js
@@ -0,0 +1,17 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    /*reset: true*/
+})
+
+
+sr.reveal('#services', { delay: 400 });
+sr.reveal('.container', { delay: 400 });
+sr.reveal('.first-line', { delay: 100 });
+sr.reveal('.second-line', { delay: 1000 });
+sr.reveal('.third-line', { delay: 100 });
+sr.reveal('.first-ebook', { delay: 100 });
+sr.reveal('.second-ebook', { delay: 1000 });
+sr.reveal('.third-ebook', { delay: 100 });
+sr.reveal('.fourth-ebook', { delay: 100 });
